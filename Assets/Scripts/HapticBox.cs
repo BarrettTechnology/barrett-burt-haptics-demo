481f211db635135052480635b6dadbfd37286ab8
Amy Blank <ab@barrett.com>
2017-01-19 10:54:12 -0500

    finished first implementation of haptic box

--- Assets/Scripts/HapticBox.cs
+++ Assets/Scripts/HapticBox.cs
@@ -3,59 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HapticBox : HapticObject {
-
-	private int entryIndex = -1;  // keeps track of which side the box was entered on
-
+	
 	void Awake () {
 		// set default values for kp and kp
 		kp = 400.0f;
 		kd = 40.0f;
 	}
 
-	// override the definition in the base class
-	void OnCollisionEnter (Collision c) {
-		Debug.Log ("Collision enter: " + this.gameObject.name);
-
-		if (c.contacts [0].otherCollider.gameObject.name == "Player") {
-			// Find which side we are closest to. That is the side we entered on.
-			// TODO: This is ugly. Find something cleaner?
-			Vector3 centerPos = this.gameObject.transform.position;
-			Vector3 contactPos = c.contacts [0].point;
-			Vector3 dims = this.gameObject.transform.localScale / 2.0f;  // distances from box center to each face
-			Vector3 depth = contactPos - centerPos;
-			float minDepth = Mathf.Abs (dims[0] - Mathf.Abs(depth [0]));
-			entryIndex = 0;
-			for (int i = 1; i < 3; i++) {
-				if (Mathf.Abs (depth [i]) < minDepth) {
-					minDepth = Mathf.Abs (dims[i] - Mathf.Abs(depth [i]));
-					entryIndex = i;
-				}
-			}
-		}
-	}
-
 	void OnCollisionStay (Collision c) {
 		if (c.contacts [0].otherCollider.gameObject.name == "Player") {
+			// Get the box size (distances from box center to each face). Assumes that
+			// the collider size is set equal to the box size, i.e., the Size parameter
+			// under the Box Collider is set to (1, 1, 1). If not, you can access the
+			// scale of the Box Collider with
+			//   c.contacts [0].thisCollider.GetComponent<BoxCollider> ().size
+			// and multiply component-wise.
+			Vector3 boxSize = this.gameObject.transform.localScale / 2.0f;
+
+			// Get the distance of the contact point from the center of this object.
 			Vector3 centerPos = this.gameObject.transform.position;
 			Vector3 contactPos = c.contacts [0].point;
-			Vector3 otherVelocity = c.contacts [0].otherCollider.gameObject.GetComponent<RobotController>().GetVelocity();
-			Vector3 dims = this.gameObject.transform.localScale / 2.0f;  // distances from box center to each face
+			Vector3 distFromCenter = contactPos - centerPos;
 
-			float depth = dims [entryIndex] - Mathf.Abs(contactPos [entryIndex] - centerPos [entryIndex]);
+			// Calculate the depth in each dimension.
+			Vector3 depth = Vector3.zero;
+			for (int i = 0; i < 3; i++) {
+				depth[i] = boxSize [i] - Mathf.Abs (distFromCenter [i]);
+			}
+
+			// Find the index of the closest side.
+			int index = 0;
+			float minDepth = depth [0];
+			for (int i = 1; i < 3; i++) {
+				if (depth [i] < minDepth) {
+					minDepth = depth [i];
+					index = i;
+				}
+			}
+
+			// Define a unit vector for the direction of the force. The direction is
+			// determined by the sign of distFromCenter, which points outward from
+			// the center of the box.
 			Vector3 direction = Vector3.zero;
-			direction [entryIndex] = Mathf.Sign (contactPos [entryIndex] - centerPos [entryIndex]);
+			direction [index] = Mathf.Sign (distFromCenter [index]);
 
-			force = -kp * depth * direction + // stiffness: pushes outward
-				-kd * Vector3.Dot (otherVelocity, direction) * direction;  // damping: pushes against radial velocity (+ or -)
-			print(force);
-			force = Vector3.zero;
+			// Calculate the stiffness force (pushes outward).
+			force = kp * depth [index] * direction;
+
+			// Get the velocity of the Player object and add the damping force, which
+			// is needed for stability. This pushes against velocity in the direction
+			// of the closest wall  (+ or -))
+			Vector3 playerVelocity = c.contacts [0].otherCollider.gameObject.GetComponent<RobotController>().GetVelocity();
+			force += -kd * Vector3.Dot (playerVelocity, direction) * direction;
 		}
-	}
-
-	// override the defintion in the base class
-	void OnCollisionExit (Collision c) {
-		Debug.Log ("Collision exit: " + this.gameObject.name);
-		force = Vector3.zero;
-		entryIndex = -1;
 	}
 }
